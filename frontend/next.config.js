241211736242d6745306d2c5b00a29beaa8496c9
qq <1042685902@qq.com>
2024-09-14 05:49:41 -0400

    refractor

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,7 +8,9 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: process.env.API_URL + '/:path*', // 使用环境变量
+        destination: process.env.API_URL 
+          ? `${process.env.API_URL}/:path*`
+          : 'http://localhost:8000/api/:path*', // 提供一个默认值
       },
     ]
   },

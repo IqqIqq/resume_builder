ef6f19707936f1222adbce2d8afd6b3a878d3614
qq <1042685902@qq.com>
2024-09-14 06:00:47 -0400

    refractor

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,4 @@
-const withTM = require('next-transpile-modules')(['@ant-design/icons', 'rc-util', 'antd', 'rc-pagination']);
+const withTM = require('next-transpile-modules')(['@ant-design/icons', 'rc-util', 'antd', 'rc-pagination', 'rc-picker']);
 const withPlugins = require('next-compose-plugins');
 
 /** @type {import('next').NextConfig} */
@@ -15,7 +15,17 @@ const nextConfig = {
       },
     ]
   },
-  transpilePackages: ['antd', '@ant-design/icons', 'rc-util', 'rc-pagination'],
+  transpilePackages: ['antd', '@ant-design/icons', 'rc-util', 'rc-pagination', 'rc-picker'],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        module: false,
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = withPlugins([withTM], nextConfig);
